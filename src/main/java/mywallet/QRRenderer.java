6e948e23a8d74f6ddf6f890a0317ed5432c50cb6
TaeGeun Moon <ahn_v3@naver.com>
2019-04-13 04:00:37 +0900

    add QR Code generator

---
+++ src/main/java/mywallet/QRRenderer.java
@@ -0,0 +1,65 @@
+package mywallet;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.EnumMap;
+import java.util.Map;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.ImageView;
+
+public class QRRenderer {
+    private String content;
+    private BufferedImage bufferedImage;
+
+    public QRRenderer(String content) {
+        setContent(content);
+    }
+
+    private void setContent(String content) {
+        this.content = content;
+        generateQRImage();
+    }
+
+    private void generateQRImage() {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        int width = 400;
+        int height = 400;
+
+        try {
+            Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.MARGIN, 2); /* default = 4 */
+            BitMatrix byteMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+            bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            bufferedImage.createGraphics();
+
+            Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(Color.BLACK);
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+
+        } catch (WriterException ex) {
+
+        }
+    }
+
+    public void displayIn(ImageView imageView) {
+        imageView.setImage(SwingFXUtils.toFXImage(bufferedImage, null));
+    }
+}
